117cc0c95b911d671d14c40b92efdd7389fb7bb9
ttand <wqazsedc@gmail.com>
2018-05-27 09:41:47 +0800

    init

---
+++ app/src/main/cpp/connect.cc
@@ -0,0 +1,5 @@
+//
+// Created by ttand on 18-5-27.
+//
+
+#include "connect.hh"
---
+++ app/src/main/cpp/connect.hh
@@ -0,0 +1,15 @@
+//
+// Created by ttand on 18-5-27.
+//
+
+#ifndef STBOX_CONNECT_HH
+#define STBOX_CONNECT_HH
+
+#include "blob.hh"
+namespace tt {
+    template <typename Dtype>
+    class connect{
+
+    };
+}
+#endif //STBOX_CONNECT_HH
