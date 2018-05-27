d31982a112ecef3038a49f299b265b07d21f6ec4
ttand <wqazsedc@gmail.com>
2018-05-27 09:13:09 +0800

    init

---
+++ app/src/main/cpp/blob.hh
@@ -0,0 +1,27 @@
+//
+// Created by ttand on 18-5-25.
+//
+
+#ifndef STBOX_BLOB_HH
+#define STBOX_BLOB_HH
+
+#include "util.hh"
+
+namespace tt {
+    template <typename Dtype>
+    class Blob{
+
+    public:
+        explicit Blob(Device device,int num,int channelNum,int height,int width){
+            shape[0] = num;
+            shape[1] = channelNum;
+            shape[2] = height;
+            shape[3] = width;
+        }
+
+    private:
+        std::array<int,4> shape;
+    };
+}
+
+#endif //STBOX_BLOB_HH
