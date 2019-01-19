0380029045c34211d8f3e4eb752143a1d2e100c0
ttand <wqazsedc@gmail.com>
2019-01-19 12:33:27 +0800

    ns

--- app/src/main/cpp/onnx.cc
+++ app/src/main/cpp/onnx.cc
@@ -8,13 +8,28 @@
 #include <iostream>
 #include <set>
 
-bool operator<(const onnx::ValueInfoProto& k1, const std::string& k2) { return k1.name() < k2; }
-bool operator<(const std::string& k1, const  onnx::ValueInfoProto& k2) { return k1 < k2.name(); }
-bool operator<(const onnx::ValueInfoProto& k1, const onnx::ValueInfoProto& k2) { return k1.name() < k2.name(); }
+struct FatKey   { int x; int data[1000]; };
+struct LightKey { int x; };
+//bool operator<(const FatKey& fk, const LightKey& lk) { return fk.x < lk.x; }
+//bool operator<(const LightKey& lk, const FatKey& fk) { return lk.x < fk.x; }
+bool operator<(const FatKey& fk1, const FatKey& fk2) { return fk1.x < fk2.x; }
+
+namespace onnx {
+    bool operator<(const onnx::ValueInfoProto &k1, const std::string &k2) { return k1.name() < k2; }
+
+    bool operator<(const std::string &k1, const onnx::ValueInfoProto &k2) { return k1 < k2.name(); }
+
+    bool operator<(const onnx::ValueInfoProto &k1, const onnx::ValueInfoProto &k2) {
+        return k1.name() < k2.name();
+    }
+}
+
+
 
 namespace tt {
+
     Onnx::Onnx(std::string fname) { //
-        int fd = open(fname.c_str(), O_RDONLY);
+        int fd = open(fname.c_str(), O_RDONLY | O_CLOEXEC);
         if (fd < 0) {
             MY_LOG(ERROR) <<"opening :"<< fname << strerror(errno) ;
             return;
@@ -29,13 +44,14 @@ namespace tt {
             MY_LOG(INFO) << "Onnxfile opset_import " <<opset.domain();
         }
         if(modelProto.has_graph()){
-            std::set<onnx::ValueInfoProto,std::less<>> inputMap;
-            auto graph = modelProto.graph();
+            std::set<onnx::ValueInfoProto, std::less<>> inputMap{};
+            auto& graph = modelProto.graph();
 
             for(auto&inputInfo:graph.input()){
                 //MY_LOG(INFO) << "Onnx Input: " << input.name();
                 //input.type().;
-                inputMap.emplace(inputInfo.name(),inputInfo);
+                inputMap.insert(inputInfo);
+//                inputMap.emplace(inputInfo);
             }
             MY_LOG(INFO) << "Onnxfile graph " << graph.name() << graph.ByteSize() << "node:" << graph.node_size() << " tensor:" << graph.initializer_size();
             for(auto&outputInfo:graph.output()){
@@ -49,7 +65,7 @@ namespace tt {
                 //MY_LOG(INFO) << "Onnx Node: " << node.name();
                 node.output();
                 for(auto&inputName:node.input()) {
-                    MY_LOG(INFO) << inputMap[inputName].DebugString();
+//                    MY_LOG(INFO) << inputMap[inputName].DebugString();
                 };
                 node.op_type();
                 node.attribute();
